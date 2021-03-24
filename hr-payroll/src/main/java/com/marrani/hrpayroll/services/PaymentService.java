488d50419ab1c216439cd78fd6c93ef8735a754e
Marrani <gabrielmarrani@gmail.com>
2021-03-23 23:21:02 -0300

    Feign client

--- hr-payroll/src/main/java/com/marrani/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/marrani/hrpayroll/services/PaymentService.java
@@ -8,24 +8,22 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.marrani.hrpayroll.feignclients.WorkerFeignClient;
 import com.marrani.hrpayroll.model.Payment;
 import com.marrani.hrpayroll.model.Worker;
 
 @Service
 public class PaymentService {
-	
-	@Value("${hr-woker.host}")
-	private String workerHost;
-	
+		
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, int days) {
 		Map<String, String> uriVariables = new HashMap<>();
 		uriVariables.put("id", ""+workerId);
 		
 		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 
